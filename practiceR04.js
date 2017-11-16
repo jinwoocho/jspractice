c49156ed050cd84dc5c893ed7d988b2a37f1b273
Jinwoo Cho <jojinwoogod@gmail.com>
2017-11-16 10:34:49 -0800

    added R04 and R05

---
+++ practiceR04.js
@@ -0,0 +1,15 @@
+<Goals and rules>
+    Return the length of the longest word in the provided sentence.
+    Your response should be a number.
+
+    <Given code>
+        function findLongestWord(str) {
+        return str.length;
+    }
+        findLongestWord(“The quick brown fox jumped over the lazy dog”);
+
+        <Testing>
+            findLongestWord(“The quick brown fox jumped over the lazy dog”) should return a number.
+            findLongestWord(“The quick brown fox jumped over the lazy dog”) should return 6.
+            findLongestWord(“May the force be with you”) should return 5.
+            findLongestWord(“Google do a barrel roll”) should return 6.
