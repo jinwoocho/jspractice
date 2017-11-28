717448f9eeb1c4c0352ae1c46c2678994e25e4ae
Jinwoo Cho <jojinwoogod@gmail.com>
2017-11-28 15:54:13 -0800

    working on 4

--- practiceR04.js
+++ practiceR04.js
@@ -1,15 +1,26 @@
-<Goals and rules>
-    Return the length of the longest word in the provided sentence.
-    Your response should be a number.
+//<Goals and rules>
+//    Return the length of the longest word in the provided sentence.
+//    Your response should be a number.
+//
+//    <Given code>
+//        function findLongestWord(str) {
+//        return str.length;
+//    }
+//        findLongestWord(“The quick brown fox jumped over the lazy dog”);
+//
+//        <Testing>
+//            findLongestWord(“The quick brown fox jumped over the lazy dog”) should return a number.
+//            findLongestWord(“The quick brown fox jumped over the lazy dog”) should return 6.
+//            findLongestWord(“May the force be with you”) should return 5.
+//            findLongestWord(“Google do a barrel roll”) should return 6.
 
-    <Given code>
-        function findLongestWord(str) {
-        return str.length;
-    }
-        findLongestWord(“The quick brown fox jumped over the lazy dog”);
+var arr = [];
 
-        <Testing>
-            findLongestWord(“The quick brown fox jumped over the lazy dog”) should return a number.
-            findLongestWord(“The quick brown fox jumped over the lazy dog”) should return 6.
-            findLongestWord(“May the force be with you”) should return 5.
-            findLongestWord(“Google do a barrel roll”) should return 6.
+function findLongestWord(str) {
+    var splitstr = str.split(" ");
+    arr = splitstr.push();
+    console.log(arr);
+    return str.length;
+
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
--- practiceR05.js
+++ practiceR05.js
@@ -1,14 +1,14 @@
-<Goals and rules>
-    Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
-
-    <Given code>
-        function titleCase(str) {
-        return str;
-    }
-        titleCase(“I’m a little tea pot”);
-
-        <Testing>
-            titleCase(“I’m a little tea pot”)should return a string.
-            titleCase(“I’m a little tea pot”)should return “I’m A Little Tea Pot”.
-            titleCase(“sHoRt AnD sToUt”) should return “Short And Stout”.
-            titleCase(“HERE IS MY HANDLE HERE IS MY SPOUT”) should return “Here Is My Handle Here Is My Spout”.
+//<Goals and rules>
+//    Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+//
+//    <Given code>
+//        function titleCase(str) {
+//        return str;
+//     }
+//         titleCase(“I’m a little tea pot”);
+//
+//        <Testing>
+//             titleCase(“I’m a little tea pot”)should return a string.
+//             titleCase(“I’m a little tea pot”)should return “I’m A Little Tea Pot”.
+//             titleCase(“sHoRt AnD sToUt”) should return “Short And Stout”.
+//             titleCase(“HERE IS MY HANDLE HERE IS MY SPOUT”) should return “Here Is My Handle Here Is My Spout”.
