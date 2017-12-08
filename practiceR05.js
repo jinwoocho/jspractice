6cbeff701fb887763e8d5c60e973334e63be9447
Jinwoo Cho <jojinwoogod@gmail.com>
2017-12-08 10:52:46 -0800

    practiceR05 runs

--- practiceR05.js
+++ practiceR05.js
@@ -12,3 +12,21 @@
 //             titleCase(“I’m a little tea pot”)should return “I’m A Little Tea Pot”.
 //             titleCase(“sHoRt AnD sToUt”) should return “Short And Stout”.
 //             titleCase(“HERE IS MY HANDLE HERE IS MY SPOUT”) should return “Here Is My Handle Here Is My Spout”.
+
+function titleCase(str){
+    lowstr = str.toLowerCase();
+    // console.log(lowstr);
+    var splitstr = lowstr.split(' ');
+    // console.log(splitstr);
+    for(var i = 0 ; i < splitstr.length; i ++){
+        var upperfirst = splitstr[i][0].toUpperCase();
+        var rest = splitstr[i].slice(1);
+        // console.log(upperfirst);
+        // console.log(rest);
+        splitstr[i] = upperfirst + rest;
+        // console.log(splitstr);
+        str = splitstr.join(' ');
+    }
+    return str;
+}
+titleCase("sHoRt AnD sToUt");
